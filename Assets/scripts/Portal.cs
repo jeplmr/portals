85bffa729f87f275cf756f1845893229cc22020f
jeplmr <jeplmr@gmail.com>
2020-02-11 21:56:50 -0600

    travel not working, timestamp 9:17

--- Assets/scripts/Portal.cs
+++ Assets/scripts/Portal.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+
 
 public class Portal : MonoBehaviour
 {
@@ -8,15 +11,64 @@ public class Portal : MonoBehaviour
     Camera portalCam; 
     RenderTexture viewTexture; 
 
+    private List<PortalTraveller> trackedTravellers; 
+
     void Awake(){
         playerCam = Camera.main; 
         portalCam = GetComponentInChildren<Camera>(); 
         portalCam.enabled = false; 
+        trackedTravellers = new List<PortalTraveller>(); 
     }
     
     void Update(){
         CreateViewTexture(); 
         Render(); 
+    }
+
+    void LateUpdate(){
+        for(int i = 0; i < trackedTravellers.Count; i++){
+            PortalTraveller traveller = trackedTravellers[i];
+            Transform travellerT = traveller.transform;
+
+            Vector3 offsetFromPortal = travellerT.position - transform.position; 
+            int portalSide = System.Math.Sign(Vector3.Dot(offsetFromPortal, transform.forward)); 
+            int portalSideOld = System.Math.Sign(Vector3.Dot(traveller.previousOffsetFromPortal, transform.forward)); 
+            //Teleport the traveller if it has crossed from one side of the portal to the other
+            if(portalSide != portalSideOld){
+                var m = linkedPortal.transform.localToWorldMatrix * transform.worldToLocalMatrix * travellerT.localToWorldMatrix; 
+                traveller.Teleport(transform, linkedPortal.transform, m.GetColumn(3), m.rotation); 
+
+                linkedPortal.OnTravellerEnterPortal(traveller);
+                trackedTravellers.RemoveAt(i);
+                i--;  
+            } else {
+                traveller.previousOffsetFromPortal = offsetFromPortal; 
+            }
+        }
+    }
+
+    void OnTravellerEnterPortal(PortalTraveller traveller){
+        if(!trackedTravellers.Contains (traveller)){
+            traveller.EnterPortalThreshold(); 
+            traveller.previousOffsetFromPortal = traveller.transform.position - transform.position; 
+            trackedTravellers.Add(traveller); 
+        }
+    }
+
+    
+    void OnTriggerEnter(Collider other){
+        var traveller = other.GetComponent<PortalTraveller>();
+        if(traveller){
+            OnTravellerEnterPortal(traveller); 
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        var traveller = other.GetComponent<PortalTraveller>();
+        if(traveller && trackedTravellers.Contains (traveller)){
+            traveller.ExitPortalThreshold(); 
+            trackedTravellers.Remove(traveller); 
+        }   
     }
 
     void CreateViewTexture(){
--- Assets/scripts/PortalTraveller.cs
+++ Assets/scripts/PortalTraveller.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PortalTraveller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public Vector3 previousOffsetFromPortal{get; set;}
+
+    public virtual void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot){
+        transform.position = pos; 
+        transform.rotation = rot; 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //called when first touches portal
+    public virtual void EnterPortalThreshold(){
     }
+
+    //called when no longer touching portal (excluding when teleporting)
+    public virtual void ExitPortalThreshold(){
+    }
+
 }
